d85e7b0be21d57136ab2b2651791062af411a2a0
Ibrahim Huseynov <tu7pdt8l8@aztu.local>
2022-06-24 19:44:14 +0400

    sass

---
+++ onepage323223/Areas/Manager/Controllers/AdminController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace onepage323223.Areas.Manager.Controllers
+{
+    [Area("Manager")]
+    public class AdminController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ onepage323223/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace onepage323223.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
